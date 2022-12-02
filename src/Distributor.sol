cb87d13bac87637fef7ae51c15c56d193d801862
Murat Saglam <muratusaglam@gmail.com>
2022-12-02 22:11:49 +0000

    Updated version with SBT with timed burning

--- src/Distributor.sol
+++ src/Distributor.sol
@@ -1,107 +1,82 @@
     // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/metatx/ERC2771Context.sol";
-import "@openzeppelin/contracts/metatx/MinimalForwarder.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Receiver.sol";
-import "@chainlink-brownie-contracts/AutomationCompatibleInterface.sol";
 
-contract characterNFT is
-    ERC2771Context,
-    ERC1155,
-    ERC1155Burnable,
-    Ownable,
-    AutomationCompatibleInterface,
-    ERC1155Supply
-{
-    uint256 public currentDate = block.timestamp;
+contract characterNFT is ERC1155, ERC1155Burnable, Ownable, ERC1155Supply {
+    uint256 public currentDate;
     uint256 public constant TRIALNFTID = 0;
-
-    address user = _msgSender();
 
     struct trialNFTInfo {
         uint256 mintDate4NFT;
         uint256 expireDate;
     }
 
-    struct accessLevel {
-        bool isExperiencedUser;
-        bool isPremiumUser;
+    enum accountPrivellege {
+        noob, // 0 default value
+        experienced, // 1
+        premium // 2
     }
 
     mapping(address => trialNFTInfo) public trialNFT;
-    mapping(address => accessLevel) public levelOfAccess;
+    mapping(address => accountPrivellege) public levelOfAccess;
 
     event NFTIsMinted(address indexed account, string message);
     event trialNFTIsMinted(address indexed account, string message);
     event Attest(address indexed to, uint256 indexed tokenId);
 
-    constructor(MinimalForwarder forwarder)
-        ERC1155("http://127.0.0.1:5500/api/characterNFT/{id}.json")
-        ERC2771Context(address(forwarder))
-    {}
+    constructor() ERC1155("http://127.0.0.1:5500/api/characterNFT/{id}.json") {
+        levelOfAccess[msg.sender] = accountPrivellege.premium;
+    }
 
-    function trialNFTMint(uint256 amount, bytes memory data) public onlyNewUser {
+    function trialNFTMint() public onlyNewUser {
         uint256 timeTillExpire = 7 days;
-
-        trialNFT[user].mintDate4NFT = block.timestamp;
-        trialNFT[user].expireDate = trialNFT[user].mintDate4NFT + timeTillExpire;
-        _mint(user, TRIALNFTID, amount, data);
-        emit trialNFTIsMinted(user, "Trial NFT is minted");
+        trialNFT[msg.sender].mintDate4NFT = block.timestamp;
+        trialNFT[msg.sender].expireDate = trialNFT[msg.sender].mintDate4NFT + timeTillExpire;
+        _mint(msg.sender, TRIALNFTID, 1, "0x11");
+        emit trialNFTIsMinted(msg.sender, "Trial NFT is minted");
     }
 
-    function mint(address addressToMint, uint256 id, uint256 amount, bytes memory data) public onlyOwner {
-        // _msgSender() is the caller of the function but the distributor is the owner of the contract
-        _mint(addressToMint, id, amount, data);
-
-        emit NFTIsMinted(user, "NFT is minted");
-    }
-
-    function checkUpkeep(bytes memory) public override returns (bool needsUpkeep, bytes memory) {
-        bool timePassed = (trialNFT[user].expireDate >= currentDate);
+    function checkUpkeep() public returns (bool needsUpkeep) {
+        currentDate = block.timestamp;
+        bool timePassed = (trialNFT[msg.sender].expireDate <= currentDate);
         needsUpkeep = (timePassed);
     }
 
     //burns token once condition is met
-    function performUpkeep(bytes calldata) external override {
-        (bool needsUpkeep,) = checkUpkeep("");
+    function performUpkeep() external {
+        (bool needsUpkeep) = checkUpkeep();
         require(needsUpkeep == true, "Upkeep not needed.");
-        _burn(_msgSender(), TRIALNFTID, 1);
+        _burn(msg.sender, TRIALNFTID, 1);
     }
 
     //testing purposes
     function resetToNewUser(address accountToReset) public onlyOwner {
-        levelOfAccess[accountToReset].isExperiencedUser = false;
+        levelOfAccess[accountToReset] = accountPrivellege.noob;
     }
 
     function makePremium(address userToMakePremium) external isExperiencedToPremium(userToMakePremium) onlyOwner {}
 
     modifier onlyNewUser() {
-        require(levelOfAccess[_msgSender()].isExperiencedUser == false, "Not a new user");
+        require(
+            levelOfAccess[_msgSender()] == accountPrivellege.noob,
+            "You're not a new user, only a new user can mint a trial NFT"
+        );
         _;
-        levelOfAccess[_msgSender()].isExperiencedUser = true;
+        levelOfAccess[_msgSender()] = accountPrivellege.experienced;
     }
 
     modifier isExperiencedToPremium(address userToMakePremium) {
         require(
-            levelOfAccess[userToMakePremium].isExperiencedUser == true
-                && levelOfAccess[userToMakePremium].isPremiumUser == false,
-            "Not an experienced user"
+            levelOfAccess[userToMakePremium] == accountPrivellege.experienced,
+            "Not an experienced user, please mint a trial NFT before continuing"
         );
         _;
-        levelOfAccess[userToMakePremium].isPremiumUser = true;
-    }
-    // The following functions are overrides required by Openzepllin to prioritise said methods.
-    function _msgSender() internal view override (Context, ERC2771Context) returns (address sender) {
-        sender = ERC2771Context._msgSender();
-    }
-
-    function _msgData() internal view override (Context, ERC2771Context) returns (bytes calldata) {
-        return ERC2771Context._msgData();
+        levelOfAccess[userToMakePremium] = accountPrivellege.premium;
     }
 
     // The following functions are overrides required by Solidity.
@@ -114,14 +89,19 @@ contract characterNFT is
         bytes memory data
     ) internal override (ERC1155, ERC1155Supply) {
         super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
-        if (levelOfAccess[_msgSender()].isPremiumUser == false) {
-            require(from == address(0), "This token can only be minted, Please upgrade to make it transferable"); // Require it to be minted
+        if (levelOfAccess[_msgSender()] != accountPrivellege.premium) {
+            require(
+                from == address(0) || to == address(0),
+                "This token can not be transferred, Please upgrade to make it transferable"
+            ); // Require it to be minted
         }
     }
 
     function _afterTokenTransfer(address from, address to, uint256[] memory ids) internal virtual {
-        if (from == address(0) && levelOfAccess[_msgSender()].isPremiumUser == false) {
+        if (from == address(0) && levelOfAccess[_msgSender()] != accountPrivellege.premium) {
             emit Attest(to, ids[0]);
+        } else if (to == address(0) && levelOfAccess[_msgSender()] != accountPrivellege.premium) {
+            emit Attest(from, ids[0]);
         }
     }
 }
